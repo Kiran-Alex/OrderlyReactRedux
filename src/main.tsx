9fa7a7340a5c98d360e0480ed402b724c917557d
Kiran-Alex <kiranalexc@gmail.com>
2024-05-08 20:03:24 +0530

    init

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { ThirdwebProvider } from '@thirdweb-dev/react'
+import { ThirdwebProvider,  metamaskWallet,
+  zerionWallet,
+  walletConnect, } from '@thirdweb-dev/react'
 import { store } from './app/store/store.ts'
 import { Provider } from 'react-redux'
 
@@ -10,7 +12,17 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
     <ThirdwebProvider
+     supportedWallets={[
+      metamaskWallet({
+        recommended: true,
+      }),
+      zerionWallet({
+        recommended: true,
+      }),
+      walletConnect(),
+    ]}
     activeChain={"arbitrum-sepolia"}
+    
     clientId={import.meta.env.VITE_THIRD_WEB_CLIENT_ID}
     >
     <App />
